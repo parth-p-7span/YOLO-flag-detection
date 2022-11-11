c059fa8e7b78e7291757249b3f0eeb7743776b59
Parth Panchal <parth.p@7span.com>
2022-11-11 17:05:42 +0530

    reformatted main.py and updated requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-
-
 from flask import jsonify, Flask, request
-from werkzeug.utils import secure_filename
 import os
 from pathlib import Path
 from yolov5.my_pred import run
 
-# weight_path = '../weights/best.pt'
 root = os.getcwd()
 weight_path = os.path.join(root, 'weights', 'best.pt')
 UPLOAD_FOLDER = os.path.join(root, "temp")
@@ -14,44 +10,44 @@ ALLOWED_EXTENSIONS = {'jpg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/file-upload', methods=['POST'])
 def upload_file():
-	# check if the post request has the file part
-	if 'file' not in request.files:
-		resp = jsonify({'message' : 'No file part in the request'})
-		resp.status_code = 400
-		return resp
-	file = request.files['file']
-	if file.filename == '':
-		resp = jsonify({'message' : 'No file selected for uploading'})
-		resp.status_code = 400
-		return resp
-	if file and allowed_file(file.filename):
-		filename = "temp.jpg"
-		temp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message': 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = "temp.jpg"
+        temp = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-		file.save(temp)
-		# function call #
+        file.save(temp)
+        # function call #
 
-		data = run(weights=Path(weight_path), source=Path(temp))
-		resp = jsonify({'message' : 'File successfully uploaded', "FLAG COLOR" : data["FLAG COLOR"], "dominant color is" : data["dominant color is"]})
-		resp.status_code = 201
-		return resp
+        data = run(weights=Path(weight_path), source=Path(temp))
+        resp = jsonify({'message': 'File successfully uploaded', "FLAG COLOR": data["FLAG COLOR"],
+                        "dominant color is": data["dominant color is"]})
+        resp.status_code = 201
+        return resp
 
-	else:
-		resp = jsonify({'message' : 'Allowed only jpg'})
-		resp.status_code = 400
-		return resp
-
-	# run(weights=Path(weight_path), source=Path(temp))
-
+    else:
+        resp = jsonify({'message': 'Allowed only jpg'})
+        resp.status_code = 400
+        return resp
 
 
+# run(weights=Path(weight_path), source=Path(temp))
 
 
-if __name__  == "__main__":
-
+if __name__ == "__main__":
     app.run(debug=True)
